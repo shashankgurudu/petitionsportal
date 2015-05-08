a08a48dae4da5093347be698776b5dd4871b11b9
Shashank <shashank@shashank.(none)>
2015-05-08 14:34:35 +0530

    disqus comments

--- petition.php
+++ petition.php
@@ -1,24 +1,45 @@
 <!DOCTYPE html>
 <html>
 <head>
-	<title>College Petitions Portal</title>
+  <title>College Petitions Portal</title>
+  
 </head>
-	<meta charset = "utf-8">
-	<meta name="viewport" content="width=device-width, initial-scale=1">
-	<title>CBIT Petitions Portal</title>
-	<!-- Latest compiled and minified CSS -->
-	<link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/css/bootstrap.min.css">
+  <meta charset = "utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <title>CBIT Petitions Portal</title>
+  <!-- Latest compiled and minified CSS -->
+  <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/css/bootstrap.min.css">
 
-	<!-- jQuery library -->
-	<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
+  <!-- jQuery library -->
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
 
-	<!-- Latest compiled JavaScript -->
-	<script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/js/bootstrap.min.js"></script>
+  <!-- Latest compiled JavaScript -->
+  <script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/js/bootstrap.min.js"></script>
+  <script src="//ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js"></script>
+  <script src="inlineDisqussions.js"></script>
     <link href="includes/style.css" rel="stylesheet">
+    <link rel="stylesheet" type="text/css" href="inlineDisqussions.css" />
+    <script>
+
+        disqus_shortname = 'changecbit';
+        jQuery(document).ready(function() {
+        jQuery("h4").inlineDisqussions({
+            identifier: '<?php $petition_no; ?>',
+              displayCount: true,
+  highlighted: false,
+  position: 'right',
+  background: 'white',
+  maxWidth: 7000
+        });
+        });
+
+  </script>
+
 <body bgcolor="#4FC3F7" >
-	<nav class="navbar navbar-default  ">
+
+  <nav class="navbar navbar-default  ">
   <div class="container-fluid">
-  	<h1>CBIT Petitions Portal</h1>
+    <h1>CBIT Petitions Portal</h1>
     <!-- Brand and toggle get grouped for better mobile display -->
     <div class="navbar-header">
       <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
@@ -61,56 +82,63 @@
 <?php
 session_start(); 
 if (isset($_SESSION['login_user']) == FALSE) {
-	header("location: login.php");
+  header("location: login.php");
 
 }
 else{
-	
-	include('connection.php');
-	$user=$_SESSION['login_user'];
-	if(isset($_GET['value']))
-	{
-		if($_GET['value']=="success")
-			echo "<script> alert('success');</script>";
-		else if($_GET['value']=="fail")
-			echo "<script> alert('fail');</script>";
-	
-		}
+  
+  include('connection.php');
+  $user=$_SESSION['login_user'];
+  if(isset($_GET['value']))
+  {
+    if($_GET['value']=="success")
+      echo "<script> alert('success');</script>";
+    else if($_GET['value']=="fail")
+      echo "<script> alert('fail');</script>";
+  
+    }
 
 
 echo "<div id='create'> 
-	<form id='loginform' name='loginid' method='post' action='cre.php'>
- 		<input  name='title' type='text' id='title'>
- 		<input  name='text' type='text' id='text'>
- 		<input  name='submit' type='submit' id='submit' value='submit'>
- 	</form>
- 	</div>";
+  <form id='loginform' name='loginid' method='post' action='cre.php'>
+    <input  name='title' type='text' id='title'>
+    <input  name='text' type='text' id='text'>
+    <input  name='submit' type='submit' id='submit' value='submit'>
+  </form>
+  </div>";
 
-	$tbval = mysql_query("SELECT * FROM peti") or die("query error"); 
-	while ($result = mysql_fetch_array($tbval)) 
-	{
-		$petition_no=$result['serial_no'];
-		$query1="SELECT * from upvotes where username='$user' and petition_no='$petition_no'";
-		$res=mysql_query($query1);	
+  $tbval = mysql_query("SELECT * FROM peti") or die("query error"); 
+  while ($result = mysql_fetch_array($tbval)) 
+  {
+    $petition_no=$result['serial_no'];
+    $query1="SELECT * from upvotes where username='$user' and petition_no='$petition_no'";
+    $res=mysql_query($query1);  
 
-		echo "<div class='jumbotron' id='text'> 
-		<h1>".$result['title']."</h1>
-		<p>".$result['text']."</p>
-		<p> Total Upvotes        ".$result['count']."</p>";
-		
-		if(mysql_num_rows($res)==0)
-		echo "<a href='count.php?id=".$petition_no."'>  <button class='btn btn-success' type='button'>
-  Upvote <span class='badge'>".$result['count']."</span>
-</button> </a>";
-		else if(mysql_num_rows($res)==1)
-		echo	"<button class='btn btn-default' type='button'> Upvoted <span class='badge'>".$result['count']."</span></button>";		
-		echo "<a href='upvote.php?id=".$petition_no."'>  upvoted by </a>";
-		echo "<p> Created by ".$result['user']."</p>";
-		echo "</div>";
-	}
-	
+    echo "<div class='jumbotron' id='text'> 
+    <h1>".$result['title']."</h1>
+    <p>".$result['text']."</p>
+    <p> Total Upvotes:".$result['count']."</p>";
+    
+    
+
+    if(mysql_num_rows($res)==0)
+    echo "<a href='count.php?id=".$petition_no."'>  <button class='btn btn-success' type='button'>
+      Upvote <span class='badge'>".$result['count']."</span>
+    </button> </a>";
+    else if(mysql_num_rows($res)==1)
+    echo  "<button class='btn btn-default' type='button'> Voted <span class='badge'>".$result['count']."</span></button>";    
+    echo "<a href='upvote.php?id=".$petition_no."'>  upvoted by </a>";
+    echo "<h4 style='margin-right:75%' > Created by ".$result['user']."</h4>";
+    echo "<div id='but' style='width:10%;height:30px;'>Show comments</div>";
+    echo "</div>";
+
+    
+  }
+  
 }
 ?>
+
+</body>
 <div class="bottom">
     <div class="container">
         <div class="col-md-12">
@@ -119,5 +147,5 @@ echo "<div id='create'>
         </div>
     </div>
 </div>
-</body>
+
 </html>
